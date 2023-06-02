c384c8a10d97d6b30b3e32790ee10e7ca06d0d65
Your Name <pavdarsh@gmail.com>
2023-06-02 23:07:35 +0530

    feat: auth and bill

---
+++ src/pages/api/bill/create.js
@@ -0,0 +1,50 @@
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "./auth/[...nextauth]";
+import { Prisma } from "@prisma/client";
+
+
+export default async (req, res) => {
+  const session = await getServerSession(req, res, authOptions);
+  if (session) {
+    // Signed in
+    const userId = session.user.id;
+    const { itemname, catname, price, total_amount } = req.body;
+    const newBill = await Prisma.bill.create({
+      data: {
+        Total_amount: total_amount,
+        User: {
+          connect: {
+            id: userId,
+          },
+        },
+      },
+    });
+    const newcat = await Prisma.category.create({
+        data:{
+            catname:catname,
+            Bill:{
+                connect: {
+                    billId: newBill.billId
+                }
+            }
+        }
+    })
+    const newItem = await Prisma.item.create({
+        data:{
+            itemname:itemname,
+            Price: price,
+            Category:{
+                connect: {
+                    catid: newcat.catid
+                }
+            }
+        }
+    })
+
+    console.log("Session", JSON.stringify(session, null, 2));
+  } else {
+    // Not Signed in
+    res.status(401);
+  }
+  res.end();
+};
---
+++ src/pages/api/bill/get.js
@@ -0,0 +1,17 @@
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "./auth/[...nextauth]"
+import { Prisma } from "@prisma/client";
+
+export default async (req, res) => {
+  const session = await getServerSession(req, res, authOptions)
+  if (session) {
+    // Signed in
+    const userId = session.user.id;
+    const data = await Prisma.bill.findMany({where:{userId:userId}})
+    console.log("Session", JSON.stringify(session, null, 2))
+  } else {
+    // Not Signed in
+    res.status(401)
+  }
+  res.end()
+}
